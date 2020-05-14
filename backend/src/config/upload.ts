6465650fd83156853fe17a3f53b08a19242d2e1a
Gabriel França <dev.gabrielfranca@gmail.com>
2020-05-13 23:29:21 -0300

    finishing backend

--- backend/src/config/upload.ts
+++ backend/src/config/upload.ts
@@ -1,19 +1,49 @@
-import multer from 'multer';
+import multer, { StorageEngine } from 'multer';
 import path from 'path';
 import ctypto from 'crypto';
 
 const tmpFolder = path.resolve(__dirname, '..', '..', 'tmp');
 
+interface IUploadConfig {
+  driver: 'disk' | 's3';
+
+  tmpFolder: string;
+
+  uploadsFolder: string;
+
+  multer: {
+    storage: StorageEngine;
+  };
+
+  config: {
+    disk: {};
+    s3: {
+      bucket: string;
+    };
+  };
+}
+
 export default {
+  driver: process.env.STORAGE_DRIVER,
+
   tmpFolder,
   uploadsFolder: path.resolve(tmpFolder, 'uploads'),
-  storage: multer.diskStorage({
-    destination: tmpFolder,
-    filename(request, file, callback) {
-      const fileHash = ctypto.randomBytes(10).toString('HEX');
-      const filename = `${fileHash}-${file.originalname}`;
 
-      return callback(null, filename);
+  multer: {
+    storage: multer.diskStorage({
+      destination: tmpFolder,
+      filename(request, file, callback) {
+        const fileHash = ctypto.randomBytes(10).toString('HEX');
+        const filename = `${fileHash}-${file.originalname}`;
+
+        return callback(null, filename);
+      },
+    }),
+  },
+
+  config: {
+    s3: {
+      bucket: process.env.STORAGE_BUCKET,
     },
-  }),
-};
+  },
+} as IUploadConfig;
